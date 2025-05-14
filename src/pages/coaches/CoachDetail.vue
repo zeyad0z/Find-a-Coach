4efcdc62fdb843bab7c890c902580ee18630cee4
zeyad0z <zeyadabdelaziz691@gmail.com>
2025-05-14 21:09:25 +0300

    handling coach id not found URL error

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,43 +1,51 @@
 <template>
   <div>
     <div v-if="isLoading">
-    <base-spinner></base-spinner>
-  </div>
-  <div v-else-if="error">
-    <h2>{{ error }}</h2>
-  </div>
-  <div v-else>
-    <section>
-      <base-card>
-        <h2>{{ fullName }}</h2>
-        <h3>${{ rate }}/hour</h3>
-      </base-card>
-    </section>
-    <section>
-      <base-card>
-        <header>
-          <h2>Interested? Reach out now!</h2>
-          <base-button link :to="contactLink">Contact</base-button>
-        </header>
-        <router-view></router-view>
-      </base-card>
-    </section>
-    <section>
-      <base-card>
-        <base-badge
-          v-for="area in areas"
-          :key="area"
-          :type="area"
-          :title="area"
-        ></base-badge>
-        <p>{{ description }}</p>
-      </base-card>
-    </section>
-  </div>
+      <base-spinner></base-spinner>
+    </div>
+    <base-card v-else-if="error">
+      <div>
+        <h2>Coach not Found!</h2>
+        <h4>
+          Maybe check out all our
+          <router-link to="/coaches">coaches</router-link>.
+        </h4>
+      </div>
+    </base-card>
+    <div v-else>
+      <section>
+        <base-card>
+          <h2>{{ fullName }}</h2>
+          <h3>${{ rate }}/hour</h3>
+        </base-card>
+      </section>
+      <section>
+        <base-card>
+          <header>
+            <h2>Interested? Reach out now!</h2>
+            <base-button link :to="contactLink">Contact</base-button>
+          </header>
+          <router-view></router-view>
+        </base-card>
+      </section>
+      <section>
+        <base-card>
+          <base-badge
+            v-for="area in areas"
+            :key="area"
+            :type="area"
+            :title="area"
+          ></base-badge>
+          <p>{{ description }}</p>
+        </base-card>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
   props: ["id"],
   data() {
