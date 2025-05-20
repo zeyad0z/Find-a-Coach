723ed7cbc3c0a7e4e3c649acd66b40816a3a45a4
zeyad0z <zeyadabdelaziz691@gmail.com>
2025-05-20 03:11:15 +0300

    Deploying

--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import CoachesList from "./pages/coaches/CoachesList.vue";
-import CoachDetail from "./pages/coaches/CoachDetail.vue";
-import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
-import ContactCoach from "./pages/requests/ContactCoach.vue";
-import RequestsReceived from "./pages/requests/RequestsReceived.vue";
-import UserAuth from "./pages/auth/UserAuth.vue";
 import NotFound from "./pages/NotFound.vue";
 import store from "./store/index.js";
+
+const CoachDetail = () => import("./pages/coaches/CoachDetail.vue");
+const CoachRegistration = () => import("./pages/coaches/CoachRegistration.vue");
+const ContactCoach = () => import("./pages/requests/ContactCoach.vue");
+const RequestsReceived = () => import("./pages/requests/RequestsReceived.vue");
+const UserAuth = () => import("./pages/auth/UserAuth.vue");
 
 const router = createRouter({
   history: createWebHistory(),
